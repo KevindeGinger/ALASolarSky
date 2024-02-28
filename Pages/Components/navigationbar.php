6afb4c54daefb444871e2bde46da16740158017c
KevindeGinger <kevindeginger@gmail.com>
2024-02-28 12:26:50 +0100

    Test

---
+++ Pages/Components/navigationbar.php
@@ -0,0 +1,9 @@
+<div class="navbar">
+    <a class="navbutton" href="./index.php">Home</a>
+    <a class="navbutton" href="../products.php">Products</a>
+    <img class="zon" src="./Assets/zon.gif">
+    <a class="navbutton" href="../dashboard.php">Dashboard</a>
+    <a class="navbutton" href="../contact.php">Contact</a>
+    
+
+</div>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include "Pages/Components/header.php";
+include "Pages/Components/navigationbar.php";
+?>
+<body>
+<div class="welkom">
+    <p>Welkom!</p>
+</div>
+
+</body>
